d778b7fe99a7b2cf264c2caaa9747da757185403
Harshavardhana Rukminiyamma Devegowda <harshavardhana.rukminiyamma.devegowda@sap.com>
2021-06-11 23:12:05 +0530

    Adding 02-vars and example-vars.yml file

---
+++ instance-launch/launch.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+COMPONENT=$1
+
+## -z validates the variable emplty, true if it is empty
+if [ -z "${COMPONENT}" ]; then
+  echo "COMPONENT Input is needed"
+  exit 1
+fi
+
+LID=lt-00ef9747f61617ee0
+LVER=4
+
+## Validate if instance is already there
+
+DNS_UPDATE() {
+  PRIVATEIP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
+  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z048208936LT81AGJ430Q --change-batch file:///tmp/record.json | jq
+}
+INSTANCE_CREATE() {
+  INSTANCE_STATE=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1)
+  if [ "${INSTANCE_STATE}" = "running" ]; then
+    echo "${COMPONENT} Instance already exists!!"
+    DNS_UPDATE
+    return 0
+  fi
+
+  if [ "${INSTANCE_STATE}" = "stopped" ]; then
+    echo "${COMPONENT} Instance already exists!!"
+    return 0
+  fi
+
+  echo -n Instance ${COMPONENT} created - IPADDRESS is
+  aws --region us-east-1 ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress  |xargs -n1
+  sleep 10
+  DNS_UPDATE
+}
+
+if [ "${1}" == "all" ]; then
+  for component in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment ; do
+    COMPONENT=$component
+    INSTANCE_CREATE
+  done
+else
+  COMPONENT=$1
+  INSTANCE_CREATE
+fi
